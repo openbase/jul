5a42fde9bb978192cdfb80c969c466c2fd500624
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2015-05-15 15:48:06 +0200

    add a new method to the string processor

--- processing/src/main/java/de/citec/jul/processing/StringProcessor.java
+++ processing/src/main/java/de/citec/jul/processing/StringProcessor.java
@@ -11,31 +11,35 @@ package de.citec.jul.processing;
  */
 public class StringProcessor {
 
-	public static String insertSpaceBetweenCamelCase(String input) {
-		String output = "";
-		String[] split = input.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
-		for (int i = 0; i < split.length; i++) {
-			output += (i+1 < split.length && !split[i].endsWith(" ")) ? split[i] + " " : split[i];
-		}
-		return output;
-	}
+    public static String insertSpaceBetweenCamelCase(String input) {
+        String output = "";
+        String[] split = input.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
+        for (int i = 0; i < split.length; i++) {
+            output += (i + 1 < split.length && !split[i].endsWith(" ")) ? split[i] + " " : split[i];
+        }
+        return output;
+    }
 
-	public static String removeDoubleWhiteSpaces(String input) {
-		return input.replaceAll("\\s+", " ");
-	}
+    public static String removeDoubleWhiteSpaces(String input) {
+        return input.replaceAll("\\s+", " ");
+    }
 
-	public static String formatHumanReadable(String input) {
-		return removeDoubleWhiteSpaces(insertSpaceBetweenCamelCase(input)).trim();
-	}
+    public static String formatHumanReadable(String input) {
+        return removeDoubleWhiteSpaces(insertSpaceBetweenCamelCase(input)).trim();
+    }
 
     public static String transformUpperCaseToCamelCase(final String input) {
         String output = "";
-        for(String component : input.split("(_| )")) {
-            if(component.isEmpty()) {
+        for (String component : input.split("(_| )")) {
+            if (component.isEmpty()) {
                 continue;
             }
             output += component.substring(0, 1).toUpperCase() + component.substring(1).toLowerCase();
         }
         return output;
+    }
+    
+    public static String replaceHyphenWithUnderscore(String input) {
+        return input.replaceAll("-", "_");
     }
 }
