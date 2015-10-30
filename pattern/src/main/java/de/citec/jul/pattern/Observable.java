6d2b4fa0667620508061edd8bf93dfa71cef6155
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2015-10-30 18:01:07 +0100

    disable event filter because of regsitry errors.

--- pattern/src/main/java/de/citec/jul/pattern/Observable.java
+++ pattern/src/main/java/de/citec/jul/pattern/Observable.java
@@ -4,11 +4,15 @@
  */
 package de.citec.jul.pattern;
 
+import de.citec.jul.exception.CouldNotPerformException;
 import de.citec.jul.exception.MultiException;
 import de.citec.jul.exception.MultiException.ExceptionStack;
 import de.citec.jul.exception.NotAvailableException;
+import de.citec.jul.exception.printer.ExceptionPrinter;
+import de.citec.jul.exception.printer.LogLevel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,6 +50,12 @@ public class Observable<T> {
             }
 
             observers.add(observer);
+            //TODO mpohling: check if this is usefull and does not interfere with any usage.
+//            try {
+//                observer.update(this, latestValue);
+//            } catch (Exception ex) {
+//                ExceptionPrinter.printHistory(new CouldNotPerformException("Initial Observer[" + observer + "] sync failed!", ex), logger, LogLevel.ERROR);
+//            }
         }
     }
 
@@ -64,18 +74,25 @@ public class Observable<T> {
     public void notifyObservers(Observable<T> source, T observable) throws MultiException {
         ExceptionStack exceptionStack = null;
 
-        if(observable == null) {
-            logger.debug("Skip notification because observable is null!");
-            return;
-        }
-        
-        if (unchangedDataFilter && latestValue != null && latestValue.equals(observable)) {
-            logger.debug("Skip notification because observable has not changed!");
-            return;
-        }
-
-        latestValue = observable;
         synchronized (LOCK) {
+            //TODO mpohling: check why this is not working!
+//            if (observable == null) {
+//                logger.debug("Skip notification because observable is null!");
+//                return;
+//            }
+//
+//            if (unchangedDataFilter && latestValue != null && latestValue.equals(observable) && latestValue.toString().equals(observable.toString())) {
+//                try {
+//                    Thread.sleep(100);
+//                } catch (InterruptedException ex) {
+//                    java.util.logging.Logger.getLogger(Observable.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//                logger.debug("Skip notification because observable has not changed!");
+//                return;
+//            }
+
+            latestValue = observable;
+
             for (Observer<T> observer : observers) {
                 try {
                     observer.update(source, observable);
