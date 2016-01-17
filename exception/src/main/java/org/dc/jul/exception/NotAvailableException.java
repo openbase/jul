1d3d46faaa243fc1810bbec61d1cfedb603f4894
Divine Threepwood <DivineThreepwood@gmail.com>
2016-01-17 21:51:07 +0100

    Implement some new constructor for NotAvailableExceptions

--- exception/src/main/java/org/dc/jul/exception/NotAvailableException.java
+++ exception/src/main/java/org/dc/jul/exception/NotAvailableException.java
@@ -11,50 +11,91 @@ package org.dc.jul.exception;
  */
 public class NotAvailableException extends InvalidStateException {
 
-    public NotAvailableException(Object object, String message) {
-        this(object.getClass(), message);
-    }
-
+    /**
+     *
+     * @param object
+     * @param cause
+     */
     public NotAvailableException(Object object, Throwable cause) {
         this(object.getClass(), cause);
     }
 
-    public NotAvailableException(Class clazz, String message) {
-        this(clazz.getSimpleName(), message);
-    }
-
+    /**
+     *
+     * @param clazz
+     * @param cause
+     */
     public NotAvailableException(Class clazz, Throwable cause) {
         this(clazz.getSimpleName(), cause);
     }
 
+    /**
+     *
+     * @param object
+     */
     public NotAvailableException(Object object) {
         this(object.getClass().getSimpleName());
     }
 
+    /**
+     *
+     * @param clazz
+     */
     public NotAvailableException(Class clazz) {
         this(clazz.getSimpleName());
     }
 
-    public NotAvailableException(String objectName, String message, Throwable cause) {
-        super("Context[" + objectName + "] is not available! " + message, cause);
-    }
-
-    public NotAvailableException(String objectName, String message) {
-        super("Context[" + objectName + "] is not available! " + message);
-    }
-
+    /**
+     *
+     * @param objectName
+     */
     public NotAvailableException(String objectName) {
         super("Context[" + objectName + "] is not available!");
     }
 
+    /**
+     *
+     * @param objectName
+     * @param cause
+     */
     public NotAvailableException(String objectName, Throwable cause) {
         super("Context[" + objectName + "] is not available!", cause);
     }
 
+    /**
+     *
+     * @param objectName
+     * @param identifiere
+     */
+    public NotAvailableException(final Class objectName, final String identifiere) {
+        super(objectName+"["+identifiere+"]] is not available!");
+    }
+
+    /**
+     *
+     * @param objectName
+     * @param identifiere
+     * @param cause
+     */
+    public NotAvailableException(final Class objectName, final String identifiere, final Throwable cause) {
+        super(objectName+"["+identifiere+"]] is not available!", cause);
+    }
+
+    /**
+     *
+     * @param cause
+     */
     public NotAvailableException(Throwable cause) {
         super(cause);
     }
 
+   /**
+    * 
+    * @param message
+    * @param cause
+    * @param enableSuppression
+    * @param writableStackTrace
+    */
     public NotAvailableException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
