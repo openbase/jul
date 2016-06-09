3c514b408ffb38d6be0b35c41be32bee2e468d9f
Divine Threepwood <DivineThreepwood@gmail.com>
2016-06-09 02:34:10 +0200

    data request log time unit fixed.

--- extension/rsb/com/src/main/java/org/dc/jul/extension/rsb/com/RSBRemoteService.java
+++ extension/rsb/com/src/main/java/org/dc/jul/extension/rsb/com/RSBRemoteService.java
@@ -371,7 +371,7 @@ public abstract class RSBRemoteService<M extends GeneratedMessage> implements RS
                 } catch (TimeoutException ex) {
                     ExceptionPrinter.printHistory(ex, logger, LogLevel.WARN);
                     timeout = generateTimeout(timeout);
-                    logger.warn("Waiting for RPCServer[" + remoteServer.getScope() + "] to call method [" + methodName + "(" + argument + ")]. Next timeout in " + ((int) timeout) + " seconds.");
+                    logger.warn("Waiting for RPCServer[" + remoteServer.getScope() + "] to call method [" + methodName + "(" + argument + ")]. Next timeout in " + ((int) (timeout/1000)) + " seconds.");
                     Thread.yield();
                 }
             }
