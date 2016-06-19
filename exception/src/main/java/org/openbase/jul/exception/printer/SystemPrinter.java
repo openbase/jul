37e0351e8975d0da05da1fa41f204381cf16ed93
Divine Threepwood <DivineThreepwood@gmail.com>
2016-06-19 22:51:45 +0200

    Implement exception printer standard out support.

---
+++ exception/src/main/java/org/openbase/jul/exception/printer/SystemPrinter.java
@@ -0,0 +1,63 @@
+package org.openbase.jul.exception.printer;
+
+/*-
+ * #%L
+ * JUL Exception
+ * %%
+ * Copyright (C) 2015 - 2016 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import java.io.PrintStream;
+import org.openbase.jps.core.JPService;
+import org.openbase.jps.exception.JPServiceException;
+import org.openbase.jps.preset.JPVerbose;
+import org.openbase.jul.exception.CouldNotPerformException;
+
+/**
+ * @author <a href="mailto:DivineThreepwood@gmail.com">Divine Threepwood</a>
+ */
+public class SystemPrinter implements Printer {
+
+    private final PrintStream printStream;
+
+    public SystemPrinter(PrintStream printStream) {
+        this.printStream = printStream;
+    }
+
+    @Override
+    public void print(String message) {
+
+       printStream.println(message);
+    }
+
+    @Override
+    public void print(String message, Throwable throwable) {
+        printStream.println(message);
+        throwable.printStackTrace(printStream);
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+        try {
+            return (JPService.getProperty(JPVerbose.class).getValue());
+        } catch (JPServiceException ex) {
+            ExceptionPrinter.printHistory(new CouldNotPerformException("Could not access java property!", ex), System.err);
+            return true;
+        }
+    }
+}
