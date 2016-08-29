10cebce0b1333d91ae2186514ac222be308af55a
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2016-08-29 10:30:09 +0200

    disable batch mode for deployment to get more error feedback.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,1 +1,2 @@
-mvn clean deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -U
+#mvn clean deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -U
+mvn clean deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -U
