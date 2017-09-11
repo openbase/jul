f32e87e2ac39aed8e35e57832b3fec94456bccbc
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-09-11 23:18:10 +0200

    xml lib added with sources

---
+++ extension/xml/src/main/java/org/openbase/jul/extension/xml/exception/NotOneNodeException.java
@@ -0,0 +1,41 @@
+package org.openbase.jul.extension.xml.exception;
+
+/*
+ * #%L
+ * JUL Extension XML
+ * %%
+ * Copyright (C) 2015 - 2017 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import nu.xom.Node;
+import nu.xom.Nodes;
+
+/**
+ *
+ @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
+ */
+public class NotOneNodeException extends XMLParsingException {
+    
+	public NotOneNodeException(final String nodeName, final Nodes nodes, final Node parent, final Exception cause) {
+		super("Found "+nodes.size()+" instead of one Node["+nodeName+"] in Element["+parent.getBaseURI()+"].", parent.getBaseURI(), cause);
+	}
+
+	public NotOneNodeException(final String nodeName, final Nodes nodes, final Node parent) {
+		super("Found "+nodes.size()+" instead of one Node["+nodeName+"] in Element["+parent.getBaseURI()+"].");
+	}
+}
---
+++ extension/xml/src/main/java/org/openbase/jul/extension/xml/exception/OverissueElementException.java
@@ -0,0 +1,41 @@
+package org.openbase.jul.extension.xml.exception;
+
+/*
+ * #%L
+ * JUL Extension XML
+ * %%
+ * Copyright (C) 2015 - 2017 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import nu.xom.Element;
+import nu.xom.Elements;
+
+/**
+ *
+ @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
+ */
+public class OverissueElementException extends XMLParsingException {
+    
+	public OverissueElementException(final String elementName, final Elements childElements, final Element parent, final Exception cause) {
+		super("Expected one Element["+elementName+"] but found " + childElements.size() + " childs of parent Element["+parent.getLocalName()+"].", cause);
+	}
+
+	public OverissueElementException(final String elementName, final Elements childElements, final Element parent) {
+		super("Expected one Element["+elementName+"] but found " + childElements.size() + " childs of parent Element["+parent.getLocalName()+"].");
+	}
+}
