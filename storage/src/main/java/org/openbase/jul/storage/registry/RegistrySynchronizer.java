09dfd1dd9f77a05013bb2b88c2e34411f7fdcf6f
Thoren Huppke <19409731+THuppke@users.noreply.github.com>
2017-10-26 14:32:24 +0200

    Fixed missing update call on localRegistry

--- storage/src/main/java/org/openbase/jul/storage/registry/RegistrySynchronizer.java
+++ storage/src/main/java/org/openbase/jul/storage/registry/RegistrySynchronizer.java
@@ -10,14 +10,14 @@ package org.openbase.jul.storage.registry;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
@@ -252,6 +252,7 @@ public class RegistrySynchronizer<KEY, ENTRY extends Configurable<KEY, CONFIG_M>
     public ENTRY update(final CONFIG_M config) throws CouldNotPerformException, InterruptedException {
         ENTRY entry = localRegistry.get(remoteRegistry.getId(config));
         entry.applyConfigUpdate(config);
+        localRegistry.update(entry);
         return entry;
     }
 
