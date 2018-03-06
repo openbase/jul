26793a5452ea67f438f736ec82702e717865dd9f
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-03-06 16:26:00 +0100

    differentiate between filter and listFilter interface

--- pattern/default/src/main/java/org/openbase/jul/pattern/Filter.java
+++ pattern/default/src/main/java/org/openbase/jul/pattern/Filter.java
@@ -22,25 +22,17 @@ package org.openbase.jul.pattern;
  * #L%
  */
 
-import java.util.List;
 import org.openbase.jul.exception.CouldNotPerformException;
+
+import java.util.List;
 
 /**
  * Filter which decides for a list of objects which to keep and which to filter out.
  *
- * @author <a href="mailto:thuxohl@techfak.uni-bielefeld.de">Tamino Huxohl</a>
  * @param <T> the type of object on which the filter works
+ * @author <a href="mailto:thuxohl@techfak.uni-bielefeld.de">Tamino Huxohl</a>
  */
 public interface Filter<T> {
-
-    /**
-     * Filter object from the list for which the verification fails.
-     *
-     * @param list the list which is filtered
-     * @return a filtered list
-     * @throws CouldNotPerformException if an error occurs while filtering
-     */
-    List<T> filter(List<T> list) throws CouldNotPerformException;
 
     /**
      * Verifies an object of type t.
@@ -49,6 +41,39 @@ public interface Filter<T> {
      * @return true if it should be kept and else false
      * @throws CouldNotPerformException if the verification fails
      */
-    boolean verify(T type) throws CouldNotPerformException;
-    
+    boolean filter(T type) throws CouldNotPerformException;
+
+    /**
+     * Verifies an object of type t.
+     *
+     * @param type the object which is verified
+     * @return true if it should be kept and else false
+     * @throws CouldNotPerformException if the verification fails
+     *
+     * @deprecated refactored to filter so use this instead
+     */
+    @Deprecated
+    default boolean verify(T type) throws CouldNotPerformException {
+        return filter(type);
+    }
+
+    /**
+     * Filter object from the list for which the verification fails.
+     *
+     * @param list the list which is filtered
+     * @return a filtered list
+     * @throws CouldNotPerformException if an error occurs while filtering
+     *
+     * @deprecated not part of filter anymore use ListFilter instead
+     */
+    @Deprecated
+    default List<T> filter(List<T> list) throws CouldNotPerformException {
+        for (int i = 0; i < list.size(); i++) {
+            if (!filter(list.get(i))) {
+                list.remove(i);
+                i--;
+            }
+        }
+        return list;
+    }
 }
