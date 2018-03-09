7a58da2fc3753101e0d715985330f35dbccb9b7f
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-03-09 18:33:44 +0100

    forgot to add new files

---
+++ extension/rsb/com/src/main/java/org/openbase/jul/extension/rsb/com/TransactionIdProvider.java
@@ -0,0 +1,38 @@
+package org.openbase.jul.extension.rsb.com;
+
+/*-
+ * #%L
+ * JUL Extension RSB Communication
+ * %%
+ * Copyright (C) 2015 - 2018 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import org.openbase.jul.exception.NotAvailableException;
+
+public interface TransactionIdProvider {
+
+    String TRANSACTION_ID_FIELD_NAME = "transaction_id";
+
+    /**
+     * Get the latest transaction id.
+     *
+     * @return the latest transaction id.
+     * @throws NotAvailableException if a transactionId is not available.
+     */
+    long getTransactionId() throws NotAvailableException;
+}
