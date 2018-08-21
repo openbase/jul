6b96337f2baeefe37bdb78bc1eab0b7f95a255fe
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-08-21 12:46:31 +0200

    remove outcommented check in ProtoBufJsonProcessor

--- extension/protobuf/src/main/java/org/openbase/jul/extension/protobuf/processing/ProtoBufJSonProcessor.java
+++ extension/protobuf/src/main/java/org/openbase/jul/extension/protobuf/processing/ProtoBufJSonProcessor.java
@@ -99,10 +99,6 @@ public class ProtoBufJSonProcessor {
             throw new InvalidStateException("Service attribute is not a protobuf message!");
         }
 
-//        if (jsonStringRep.isEmpty() || jsonStringRep.equals(EMPTY_MESSAGE)) {
-//            throw new InvalidStateException("Service attribute does not contain any context!");
-//        }
-
         return jsonStringRep;
     }
 
