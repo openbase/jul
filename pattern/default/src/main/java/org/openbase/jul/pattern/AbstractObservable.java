3e80f6cbcd1d77fe78bfd7bbc0b789d21ed8cc4f
Divine Threepwood <divine@openbase.org>
2018-09-14 14:36:35 +0200

    cleanup

--- pattern/default/src/main/java/org/openbase/jul/pattern/AbstractObservable.java
+++ pattern/default/src/main/java/org/openbase/jul/pattern/AbstractObservable.java
@@ -292,9 +292,8 @@ public abstract class AbstractObservable<S, T> implements Observable<S, T> {
 
                 }
             }
-            if (exceptionStack != null && !exceptionStack.isEmpty()) {
-                MultiException.checkAndThrow("Could not notify Data[" + observable + "] to all observer!", exceptionStack);
-            }
+            MultiException.checkAndThrow("Could not notify Data[" + observable + "] to all observer!", exceptionStack);
+
             wholeTime = System.currentTimeMillis() - wholeTime;
             if (wholeTime > 500) {
                 LOGGER.warn("Notification on observable[" + observable.getClass().getName() + "] took: " + wholeTime + "ms");
