30e65e518a5a95ae229955c7cb7902b8450d491b
Divine Threepwood <divine@openbase.org>
2018-10-03 15:13:57 +0200

    Implement defaul constructor and add javadoc

--- exception/src/main/java/org/openbase/jul/exception/InitializationException.java
+++ exception/src/main/java/org/openbase/jul/exception/InitializationException.java
@@ -10,12 +10,12 @@ package org.openbase.jul.exception;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -23,15 +23,37 @@ package org.openbase.jul.exception;
  */
 
 /**
- *
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
  */
 public class InitializationException extends CouldNotPerformException {
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param message {@inheritDoc}
+     * @param cause   {@inheritDoc}
+     */
+    public InitializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Creates a new InitializationException instance.
+     *
+     * @param context the instance or type which could not be initialized.
+     * @param cause   the reason why the initialization failed.
+     */
     public InitializationException(Object context, Throwable cause) {
         this(context.getClass(), cause);
     }
+
+    /**
+     * Creates a new InitializationException instance.
+     *
+     * @param context the class which could not be initialized.
+     * @param cause   the reason why the initialization failed.
+     */
     public InitializationException(Class context, Throwable cause) {
-        super("Could not initialize "+context+"!", cause);
+        super("Could not initialize " + context + "!", cause);
     }
 }
