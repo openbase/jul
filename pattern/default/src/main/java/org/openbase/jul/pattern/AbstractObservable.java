e38d611b5980291c18aaa8b72b77806fd203c3d9
Divine Threepwood <divine@openbase.org>
2018-10-08 10:54:34 +0200

    limit error printing to 80 chars.

--- pattern/default/src/main/java/org/openbase/jul/pattern/AbstractObservable.java
+++ pattern/default/src/main/java/org/openbase/jul/pattern/AbstractObservable.java
@@ -10,12 +10,12 @@ package org.openbase.jul.pattern;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -293,7 +293,13 @@ public abstract class AbstractObservable<S, T> implements Observable<S, T> {
                 }
             }
 
-            MultiException.checkAndThrow(() ->"Could not notify Data[" + observable + "] to all observer!", exceptionStack);
+            MultiException.checkAndThrow(() -> {
+                String stringRep = observable.toString();
+                if (stringRep.length() > 80) {
+                    stringRep = stringRep.substring(0, 80) + " [...]";
+                }
+                return "Could not notify Data[" + stringRep + "] to all observer!";
+            }, exceptionStack);
 
             wholeTime = System.currentTimeMillis() - wholeTime;
             if (wholeTime > 500) {
