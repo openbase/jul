b2424ecc72e98adf6090f31d7fb0ca85ca1c64c4
Divine Threepwood <divine@openbase.org>
2019-02-23 17:25:46 +0100

    avoid ConcurrentModificationException

--- extension/rsb/com/src/main/java/org/openbase/jul/extension/rsb/com/AbstractRemoteClient.java
+++ extension/rsb/com/src/main/java/org/openbase/jul/extension/rsb/com/AbstractRemoteClient.java
@@ -10,12 +10,12 @@ package org.openbase.jul.extension.rsb.com;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -574,32 +574,33 @@ public abstract class AbstractRemoteClient<M extends Message> implements RSBRemo
         try {
             synchronized (maintainerLock) {
                 reinitStackTraces.add(stackTraceElement);
-
-                if (reinitStackTraces.size() > 1) {
-                    for (final StackTraceElement[] trace : reinitStackTraces) {
-                        StackTracePrinter.printStackTrace("Dublicated reinit call by:", trace, logger, LogLevel.WARN);
-                    }
-                    throw new FatalImplementationErrorException("dublicated reinit detected!", this);
-                }
-                logger.debug("Reinit " + this);
-                // temporally unlock this remove but save maintainer
-                final Object currentMaintainer = maintainer;
                 try {
-                    maintainer = null;
+                    if (reinitStackTraces.size() > 1) {
+                        for (final StackTraceElement[] trace : reinitStackTraces) {
+                            StackTracePrinter.printStackTrace("Duplicated reinit call by:", trace, logger, LogLevel.WARN);
+                        }
+                        throw new FatalImplementationErrorException("Duplicated reinit detected!", this);
+                    }
+                    logger.debug("Reinit " + this);
+                    // temporally unlock this remove but save maintainer
+                    final Object currentMaintainer = maintainer;
+                    try {
+                        maintainer = null;
 
-                    // reinit remote
-                    internalInit(scope, RSBSharedConnectionConfig.getParticipantConfig());
-                } catch (final CouldNotPerformException ex) {
-                    throw new CouldNotPerformException("Could not reinit " + this + "!", ex);
+                        // reinit remote
+                        internalInit(scope, RSBSharedConnectionConfig.getParticipantConfig());
+                    } catch (final CouldNotPerformException ex) {
+                        throw new CouldNotPerformException("Could not reinit " + this + "!", ex);
+                    } finally {
+                        // restore maintainer
+                        maintainer = currentMaintainer;
+                    }
                 } finally {
-                    // restore maintainer
-                    maintainer = currentMaintainer;
+                    reinitStackTraces.remove(stackTraceElement);
                 }
             }
         } catch (final CouldNotPerformException ex) {
             throw new CouldNotPerformException("Could not reinitialize " + this + "!", ex);
-        } finally {
-            reinitStackTraces.remove(stackTraceElement);
         }
     }
 
