610498ae7ac29b32e756c70ceee103c99a717f0d
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2019-03-27 22:22:30 +0100

    add currently failing test for ProtoBufFileProcessor

---
+++ extension/protobuf/src/test/java/org/openbase/jul/extension/protobuf/processing/ProtobufFileProcessorTest.java
@@ -0,0 +1,41 @@
+package org.openbase.jul.extension.protobuf.processing;
+
+import com.google.protobuf.ByteString;
+import org.junit.Test;
+import org.openbase.type.domotic.authentication.LoginCredentialsType.LoginCredentials;
+import org.openbase.type.domotic.authentication.LoginCredentialsType.LoginCredentials.Builder;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author <a href="mailto:pleminoq@openbase.org">Tamino Huxohl</a>
+ */
+public class ProtobufFileProcessorTest {
+
+    @Test
+    public void testByteArraySerialization() throws Exception {
+        final ProtoBufFileProcessor<LoginCredentials, LoginCredentials, Builder> fileProcessor = new ProtoBufFileProcessor<>(LoginCredentials.newBuilder());
+
+        final File testFile = new File("/tmp/byteArraySerialization");
+        final String password = "12345678";
+        final LoginCredentials loginCredentials = LoginCredentials.newBuilder()
+                .setId("user_id")
+                .setCredentials(ByteString.copyFrom(hash(password))).build();
+        fileProcessor.serialize(loginCredentials, testFile);
+
+        assertEquals(loginCredentials, fileProcessor.deserialize(testFile));
+    }
+
+    private byte[] hash(String toHash) throws Exception {
+        byte[] key = toHash.getBytes(StandardCharsets.UTF_8);
+        String HASH_ALGORITHM = "SHA-256";
+        MessageDigest sha = MessageDigest.getInstance(HASH_ALGORITHM);
+        key = sha.digest(key);
+        return Arrays.copyOf(key, 16);
+    }
+}
