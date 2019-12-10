836700c4f83be3f3da8b058cd949070b22a1173f
Divine Threepwood <divine@openbase.org>
2019-12-10 18:00:14 +0100

    improve shutdown exception handling and remove outdated header.

--- visual/javafx/src/main/java/org/openbase/jul/visual/javafx/iface/DynamicPane.java
+++ visual/javafx/src/main/java/org/openbase/jul/visual/javafx/iface/DynamicPane.java
@@ -1,21 +1,3 @@
-/**
- * ==================================================================
- *
- * This file is part of org.openbase.bco.bcozy.
- *
- * org.openbase.bco.bcozy is free software: you can redistribute it and modify
- * it under the terms of the GNU General Public License (Version 3)
- * as published by the Free Software Foundation.
- *
- * org.openbase.bco.bcozy is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with org.openbase.bco.bcozy. If not, see <http://www.gnu.org/licenses/>.
- * ==================================================================
- */
 package org.openbase.jul.visual.javafx.iface;
 
 /*-
@@ -42,12 +24,13 @@ package org.openbase.jul.visual.javafx.iface;
 
 import javafx.application.Platform;
 import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.ExceptionProcessor;
 import org.openbase.jul.exception.printer.ExceptionPrinter;
 import org.slf4j.LoggerFactory;
 
 /**
  * Interface can be used for non static panes to provide a unique interface for updating the content.
- * 
+ *
  * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
  */
 public interface DynamicPane extends StaticPane {
@@ -62,7 +45,9 @@ public interface DynamicPane extends StaticPane {
             try {
                 updateDynamicContent();
             } catch (CouldNotPerformException ex) {
-                ExceptionPrinter.printHistory(new CouldNotPerformException("Could not update all dynamic components!", ex), LoggerFactory.getLogger(getClass()));
+                if (!ExceptionProcessor.isCausedBySystemShutdown(ex)) {
+                    ExceptionPrinter.printHistory(new CouldNotPerformException("Could not update all dynamic components!", ex), LoggerFactory.getLogger(getClass()));
+                }
             }
             return;
         }
@@ -70,7 +55,9 @@ public interface DynamicPane extends StaticPane {
             try {
                 updateDynamicContent();
             } catch (CouldNotPerformException ex) {
-                ExceptionPrinter.printHistory(new CouldNotPerformException("Could not update all dynamic components!", ex), LoggerFactory.getLogger(getClass()));
+                if (!ExceptionProcessor.isCausedBySystemShutdown(ex)) {
+                    ExceptionPrinter.printHistory(new CouldNotPerformException("Could not update all dynamic components!", ex), LoggerFactory.getLogger(getClass()));
+                }
             }
         });
     }
