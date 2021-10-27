b369a616939f2fd340ddd5e6a923012514ddf9af
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2021-10-27 14:11:21 +0200

    fix RPCMethod and its test

--- communication/mqtt/src/main/java/org/openbase/jul/communication/mqtt/RPCMethod.kt
+++ communication/mqtt/src/main/java/org/openbase/jul/communication/mqtt/RPCMethod.kt
@@ -3,7 +3,6 @@ package org.openbase.jul.communication.mqtt
 import com.google.protobuf.Message
 import org.openbase.jul.exception.CouldNotPerformException
 import org.openbase.type.communication.mqtt.PrimitiveType.Primitive
-import java.lang.Class
 import kotlin.Any
 import kotlin.Boolean
 import kotlin.Double
@@ -16,39 +15,46 @@ import kotlin.reflect.KFunction
 import kotlin.reflect.full.isSuperclassOf
 import com.google.protobuf.Any as protoAny
 
-class RPCMethod(private val function: KFunction<*>, private val instance: Any) {
+/**
+ * Wrap a function to be callable as a remote procedure call (RPC).
+ * To do this, the arguments and the return value of the function
+ * are wrapped in the [protobuf any type][com.google.protobuf.Any].
+ * As a result, arguments and return values have to either be protobuf
+ * messages or certain primitive values as defined in the type
+ * [Primitive][org.openbase.type.communication.mqtt.PrimitiveType.Primitive].
+ *
+ * Note: the instance parameter is only required if the function is resolved
+ * from a class (Class::function) and not from an instance of the class (instance::function).
+ * In the first case the function expects the first argument to be the instance,
+ * in the second case the function knows of the instance to be called on.
+ *
+ * @param function the function to be wrapped
+ * @param instance the instance the function is called on, is only required
+ */
+class RPCMethod(private val function: KFunction<*>, private val instance: Any = noInstance) {
 
     companion object {
+        /**
+         * Value indicating that no instance is required to call the
+         * wrapped function.
+         */
+        private const val noInstance = false
+
         /**
          * Create a function which converts an any type of the given class
          * to a proto any type.
          *
-         * This only works for messages and certain java primitive types as
-         * defined in the proto type Primitive.
+         * This only works for messages and certain primitive types as
+         * defined in [org.openbase.type.communication.mqtt.PrimitiveType.Primitive].
          */
         fun anyToProtoAny(clazz: KClass<*>): (Any) -> protoAny {
-            println("Create converter for ${clazz.qualifiedName} to protoAny...")
-
             if (Message::class.isSuperclassOf(clazz)) {
                 return { msg: Any -> protoAny.pack(msg as Message) }
             }
 
-            // Note: if a method is resolved via reflections Void::class.java does not match
-            //       therefore, we perform a string comparison here
-            /*if (clazz.qualifiedName == "void") {
-                return { _: Any? -> protoAny.getDefaultInstance() }
-            }*/
-
             return when (clazz) {
                 Int::class -> { msg: Any ->
-                    println("Call converter for int class with $msg")
-                    println("Cast ${msg as Int}")
-                    val b = Primitive.newBuilder()
-                    b.int = msg as Int
-                    print("Hallo?")
-                    print("Builder ${b.isInitialized} msg ${b.build().isInitialized}")
-                    //println("As primitive ${Primitive.newBuilder().setInt(msg as Int).build()}")
-                    protoAny.pack(b.build())
+                    protoAny.pack(Primitive.newBuilder().setInt(msg as Int).build())
                 }
                 Float::class -> { msg: Any ->
                     protoAny.pack(Primitive.newBuilder().setFloat(msg as Float).build())
@@ -65,7 +71,7 @@ class RPCMethod(private val function: KFunction<*>, private val instance: Any) {
                 Boolean::class -> { msg: Any ->
                     protoAny.pack(Primitive.newBuilder().setBoolean(msg as Boolean).build());
                 }
-                Unit::class -> {_ -> protoAny.getDefaultInstance()}
+                Unit::class -> { _ -> protoAny.getDefaultInstance() }
 
                 else -> throw CouldNotPerformException("Cannot parse class ${clazz.qualifiedName} to proto any!");
             }
@@ -75,8 +81,8 @@ class RPCMethod(private val function: KFunction<*>, private val instance: Any) {
          * Create a function which converts a proto any type to a type
          * of the given class.
          *
-         * This only works for messages and certain java primitive types as
-         * defined in the proto type Primitive.
+         * This only works for messages and certain primitive types as
+         * defined in [org.openbase.type.communication.mqtt.PrimitiveType.Primitive].
          */
         fun protoAnyToAny(clazz: KClass<*>): (protoAny) -> Any {
             if (Message::class.isSuperclassOf(clazz)) {
@@ -84,38 +90,88 @@ class RPCMethod(private val function: KFunction<*>, private val instance: Any) {
             }
 
             return when (clazz) {
-                Int::class -> { msg: protoAny -> msg.unpack(Primitive::class.java).int }
-                Float::class -> { msg: protoAny -> msg.unpack(Primitive::class.java).float }
-                Double::class -> { msg: protoAny -> msg.unpack(Primitive::class.java).double }
-                Long::class -> { msg: protoAny -> msg.unpack(Primitive::class.java).long }
-                String::class -> { msg: protoAny -> msg.unpack(Primitive::class.java).string }
-                Boolean::class -> { msg: protoAny -> msg.unpack(Primitive::class.java).boolean }
-                Unit::class -> {_ -> protoAny.getDefaultInstance()}
+                Int::class -> { msg: protoAny ->
+                    val unpacked: Primitive = msg.unpack(Primitive::class.java)
+                    if (!unpacked.hasInt()) {
+                        throw CouldNotPerformException("Arg is not of type Int!")
+                    }
+                    unpacked.int
+                }
+                Float::class -> { msg: protoAny ->
+                    val unpacked: Primitive = msg.unpack(Primitive::class.java)
+                    if (!unpacked.hasFloat()) {
+                        throw CouldNotPerformException("Arg is not of type Float!")
+                    }
+                    unpacked.float
+                }
+                Double::class -> { msg: protoAny ->
+                    val unpacked: Primitive = msg.unpack(Primitive::class.java)
+                    if (!unpacked.hasDouble()) {
+                        throw CouldNotPerformException("Arg is not of type Double!")
+                    }
+                    unpacked.double
+                }
+                Long::class -> { msg: protoAny ->
+                    val unpacked: Primitive = msg.unpack(Primitive::class.java)
+                    if (!unpacked.hasLong()) {
+                        throw CouldNotPerformException("Arg is not of type Long!")
+                    }
+                    unpacked.long
+                }
+                String::class -> { msg: protoAny ->
+                    val unpacked: Primitive = msg.unpack(Primitive::class.java)
+                    if (!unpacked.hasString()) {
+                        throw CouldNotPerformException("Arg is not of type String!")
+                    }
+                    unpacked.string
+                }
+                Boolean::class -> { msg: protoAny ->
+                    val unpacked: Primitive = msg.unpack(Primitive::class.java)
+                    if (!unpacked.hasBoolean()) {
+                        throw CouldNotPerformException("Arg is not of type Boolean!")
+                    }
+                    unpacked.boolean
+                }
+                Unit::class -> { _ -> }
 
                 else -> throw CouldNotPerformException("Cannot parse class ${clazz.qualifiedName} from proto any!")
             }
         }
     }
 
-    private val parameterParserList: List<(protoAny) -> Any> =
-        function.parameters.map { parameter -> protoAnyToAny(parameter.type.classifier as KClass<*>) }
+    private val callOnInstance = instance != noInstance
+
     private var resultParser: (Any) -> protoAny = anyToProtoAny(function.returnType.classifier as KClass<*>)
+    private var parameterParserList: List<(protoAny) -> Any>
 
     fun invoke(args: List<protoAny>): protoAny {
         if (args.size != parameterParserList.size) {
             throw CouldNotPerformException("Invalid number of arguments! Expected ${parameterParserList.size} but got ${args.size}")
         }
 
-        val parameters = args
+        var parameters = args
             .zip(parameterParserList)
             .map { (parameter, parameterParser) -> parameterParser(parameter) }
-            //.filter { parameter -> parameter == protoAny.getDefaultInstance() }
             .toTypedArray()
+        // prepend instance if necessary for the function call
+        if (callOnInstance) {
+            parameters = arrayOf(instance, *parameters)
+        }
 
-        println("Got ${parameters.size}")
-        parameters.forEach { arg -> println(arg) }
-
-        val result = function.call(instance, *parameters)!!
+        val result = function.call(*parameters)!!
         return resultParser(result);
+    }
+
+    init {
+        var filteredParameters = function.parameters
+
+        // if called on an instance the first parameter of the function is the
+        // instance and, it does not need to be wrapped
+        if (callOnInstance) {
+            filteredParameters = filteredParameters.subList(1, filteredParameters.size)
+        }
+
+        parameterParserList = filteredParameters
+            .map { parameter -> protoAnyToAny(parameter.type.classifier as KClass<*>) }
     }
 }
