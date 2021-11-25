48dc95bda01e99647f1927c720339ae18045123e
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2021-11-25 10:02:01 +0100

    exclude junit4 transitive dependency through testcontainers in communication/controller and add workaround

---
+++ communication/controller/src/test/java/org/junit/rules/TestRule.java
@@ -0,0 +1,31 @@
+package org.junit.rules;
+
+/*-
+ * #%L
+ * JUL Extension Controller
+ * %%
+ * Copyright (C) 2015 - 2021 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+//Note: workaround to exclude junit4 transitive dependency
+// https://github.com/testcontainers/testcontainers-java/issues/970#issuecomment-625044008
+// remove when testcontainers is updated
+@SuppressWarnings("unused")
+public interface TestRule {
+}
+
