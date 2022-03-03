dc67c1675f1dffe8a6f2872d1076bdb7111636c0
Divine Threepwood <divine@openbase.org>
2022-03-03 12:55:13 +0100

    fix javafx deps

--- visual/swing/build.gradle.kts
+++ visual/swing/build.gradle.kts
@@ -12,9 +12,9 @@ dependencies {
     api(project(":jul.schedule"))
     api("org.netbeans.external:AbsoluteLayout:RELEASE100")
     api(project(":jul.extension.type.interface"))
-    api("org.openjfx:javafx-swing:16")
-    api("org.openjfx:javafx-swing:16")
-    api("org.openjfx:javafx-swing:16")
+    api("org.openjfx:javafx-swing:17:win")
+    api("org.openjfx:javafx-swing:17:mac")
+    api("org.openjfx:javafx-swing:17:linux")
 }
 
 description = "JUL Visual Swing"
