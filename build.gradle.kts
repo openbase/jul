60912143c6a0a2304baf89a636d6b8fac028e8f3
Divine Threepwood <divine@openbase.org>
2022-03-30 16:22:39 +0200

    manually declare version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    id("fr.brouillard.oss.gradle.jgitver") version "0.9.1"
 }
 
 group = "org.openbase"
-version = rootProject.file("version.txt").readText().trim()
+version = "3.2-SNAPSHOT"
+//rootProject.file("version.txt").readText().trim()
 
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/"))
-            snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
+//            nexusUrl.set(uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/"))
+//            snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
             username.set(findProperty("MAVEN_CENTRAL_USERNAME")?.let { it as String? })
             password.set(findProperty("MAVEN_CENTRAL_TOKEN")?.let { it as String? })
         }
