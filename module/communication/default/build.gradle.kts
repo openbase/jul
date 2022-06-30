403057445b78fdc2cff0dde3ba9700fbdda3a7ca
Divine Threepwood <divine@openbase.org>
2022-06-30 21:22:26 +0200

    apply automated refresh version plugin updates.

--- module/communication/default/build.gradle.kts
+++ module/communication/default/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     api(project(":jul.extension.type.processing"))
     api(project(":jul.interface"))
     api(project(":jul.exception"))
-    api("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
+    api("org.jetbrains.kotlin:kotlin-reflect:_")
 }
 
 description = "JUL Communication Default"
