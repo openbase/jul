be6a7623f252c5aac45c79983cbdf6ef71c999e1
Divine Threepwood <divine@openbase.org>
2022-07-21 20:02:31 +0200

    cleanup code

--- module/communication/default/src/test/java/org/openbase/jul/communication/exception/RPCResolvedExceptionTest.kt
+++ module/communication/default/src/test/java/org/openbase/jul/communication/exception/RPCResolvedExceptionTest.kt
@@ -9,37 +9,40 @@ import org.openbase.jul.exception.printer.ExceptionPrinter
 
 internal class RPCResolvedExceptionTest {
 
+    companion object {
+        const val missingEntity = "mize"
+        const val rootMessage = "root cause"
+        const val secondLevelMessage = "second level cause"
+    }
+
     @Test
     fun testExceptionResolution() {
 
-        val missingEntity = "mize"
 
         val testException = CouldNotPerformException(
-            message = "root cause",
+            message = rootMessage,
             cause = CouldNotPerformException(
-                message = "second level cause",
+                message = secondLevelMessage,
                 cause = NotAvailableException(missingEntity)
             )
         )
 
-        val rpcException = RPCException(message = testException.stackTraceToString())
+        RPCException(message = testException.stackTraceToString()).let { rpcException ->
 
-        RPCResolvedException
-            .resolveRPCException(rpcException)
-            .let { result ->
-                ExceptionProcessor.getInitialCause(result).let { initialCause ->
-                    Assertions.assertEquals(initialCause.message, NotAvailableException(missingEntity).message)
+            RPCResolvedException
+                .resolveRPCException(rpcException)
+                .let { result ->
+                    ExceptionProcessor.getInitialCause(result).let { initialCause ->
+                        Assertions.assertEquals(initialCause.message, NotAvailableException(missingEntity).message)
+                    }
                 }
-            }
 
-        RPCResolvedException(rpcException)
-            .let { result ->
-                ExceptionProcessor.getInitialCause(result).let { initialCause ->
-                    Assertions.assertEquals(initialCause.message, NotAvailableException(missingEntity).message)
-
-                    println(result.stackTraceToString())
-                    ExceptionPrinter.printHistory(result, System.out)
+            RPCResolvedException(rpcException)
+                .let { result ->
+                    ExceptionProcessor.getInitialCause(result).let { initialCause ->
+                        Assertions.assertEquals(initialCause.message, NotAvailableException(missingEntity).message)
+                    }
                 }
-            }
+        }
     }
 }
