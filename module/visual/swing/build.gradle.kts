a637c0d91be75e3877f1d4431c5a50dd835add15
Divine Threepwood <divine@openbase.org>
2023-11-20 23:39:57 +0100

    port javafx 17 to latest version.

--- module/visual/swing/build.gradle.kts
+++ module/visual/swing/build.gradle.kts
@@ -6,9 +6,9 @@ dependencies {
     api(project(":jul.schedule"))
     api("org.netbeans.external:AbsoluteLayout:_")
     api(project(":jul.extension.type.interface"))
-    api("org.openjfx:javafx-swing:17:win")
-    api("org.openjfx:javafx-swing:17:mac")
-    api("org.openjfx:javafx-swing:17:linux")
+    api("org.openjfx:javafx-swing:17.0.9:win")
+    api("org.openjfx:javafx-swing:17.0.9:mac")
+    api("org.openjfx:javafx-swing:17.0.9:linux")
 }
 
 description = "JUL Visual Swing"
