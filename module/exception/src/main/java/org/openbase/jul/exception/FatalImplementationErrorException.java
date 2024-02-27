57f9689153d476d593e2fc9cfaad980cddbe67d5
Divine Threepwood <divine@openbase.org>
2024-02-27 22:00:45 +0100

    Improve exception handling tooling.

--- module/exception/src/main/java/org/openbase/jul/exception/FatalImplementationErrorException.java
+++ module/exception/src/main/java/org/openbase/jul/exception/FatalImplementationErrorException.java
@@ -10,12 +10,12 @@ package org.openbase.jul.exception;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -78,9 +78,17 @@ public class FatalImplementationErrorException extends CouldNotPerformException 
     }
 
     /**
+     * Default constructor that can be used to restore a fatal implementation error from a serialized state.
+     */
+    public FatalImplementationErrorException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
      * Method detects the class of the given instance. In case the instance itself is the class these one is directly returned.
      *
      * @param object
+     *
      * @return
      */
     private Class detectClass(final Object object) {
