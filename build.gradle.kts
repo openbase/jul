624961115af6d25863de263b70c857320d0fc7a4
Divine Threepwood <divine@openbase.org>
2025-10-09 23:06:11 +0200

    try again to fix maven url

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ nexusPublishing {
         sonatype {
             username.set(findProperty("MAVEN_CENTRAL_USERNAME")?.let { it as String? })
             password.set(findProperty("MAVEN_CENTRAL_TOKEN")?.let { it as String? })
-            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
             snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
         }
     }
