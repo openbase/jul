a976a1883b4d916f1c5217535dc4c23de52c39f9
Divine Threepwood <divine@openbase.org>
2025-10-09 22:08:46 +0200

    switch to new central maven setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ nexusPublishing {
         sonatype {
             username.set(findProperty("MAVEN_CENTRAL_USERNAME")?.let { it as String? })
             password.set(findProperty("MAVEN_CENTRAL_TOKEN")?.let { it as String? })
+            nexusUrl.set(uri("https://central.sonatype.com/api/v1/publisher"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
         }
     }
 }
